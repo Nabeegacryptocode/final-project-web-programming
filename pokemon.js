c633743e2d0702b8f307c879d70e0983208eafa9
Nabeegacryptocode <82228665+Nabeegacryptocode@users.noreply.github.com>
2021-12-01 23:22:42 -0500

    Add files via upload
    this is the most recent commit

--- pokemon.js
+++ pokemon.js
@@ -1,6 +1,29 @@
+
+import Backdrop from './background';
+
+
+import Pokemoninfooverlay from "./pokemoninfo";
+import { useState } from 'react';
+
+
 function PokemonPage(props){
+  const [showModal, setShowModal] = useState();
+
+  function Pokemoninfooverlayopen() {
+    setShowModal(true);
+  }
+
+  function Pokemoninfooverlayclose() {
+    setShowModal(false);
+  }
+
     return (<div>
-        <button></button>
-    </div>
+        <button className="poke" onClick={Pokemoninfooverlayopen}>
+     <img className="pic" src={props.image} alt={props.description} />
+       </button>
+       {showModal && <Backdrop onClick={Pokemoninfooverlayclose} />}
+      {showModal && <Pokemoninfooverlay text={props.info} onClose={Pokemoninfooverlayclose} />}
+       </div>
+    
     );}
 export default PokemonPage;
