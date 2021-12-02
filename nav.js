ed50598f0e71615208b58faf46be460d6dbc2255
MFaizanK23 <95009334+MFaizanK23@users.noreply.github.com>
2021-12-02 15:47:00 -0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,12 @@
+import './nav.css';
+function Navbar(){
+    return (
+        <div class="topnav" id="myTopnav">
+            <p>Home</p>
+            <p>Roster</p>
+            <p>Add Pokemon</p>
+            
+            
+        </div>);
+}
+export default Navbar;
